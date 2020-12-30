689f90864e262d1a328851a1a712bd090e4b8a8b
lovely_kite <47591097+YeonKang@users.noreply.github.com>
2020-12-31 05:02:30 +0900

    Update Lec1-1_Pythonic_Code.py

--- Lec1-1_Pythonic_Code.py
+++ Lec1-1_Pythonic_Code.py
@@ -41,8 +41,41 @@ for i in range(10):
 
 print(result)
 
-#list complrehension
+#list Complrehension
 
 result = [i for i in range(10)]
 print(result)
 
+result = [i for i in range(10) if i % 2 == 0]
+print(result)
+
+#Nested For loop
+
+word_1 = "Hello"
+word_2 = "World"
+result = [i+j for i in word_1 for j in word_2]
+
+print(result)
+
+#Nested For loop + if
+
+case_1 = ["A","B","C"]
+case_2 = ["D","E","A"]
+
+result = [i+j for i in case_1 for j in case_2]
+print(result)
+
+result = [i+j for i in case_1 for j in case_2 if not(i==j)]
+result.sort()
+
+print(result)
+
+#split + list Comprehension
+
+words = 'The quick brown fox jumps over the lazy dog'.split()
+print(words)
+
+stuff = [[w.upper(),w.lower(),len(w)] for w in words]
+
+for i in stuff:
+    print(i)
