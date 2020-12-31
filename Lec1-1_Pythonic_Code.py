6648471228c5692b3de2668d0e5cb1f4e8ead9ec
lovely_kite <47591097+YeonKang@users.noreply.github.com>
2021-01-01 03:36:19 +0900

    Update Lec1-1_Pythonic_Code.py

--- Lec1-1_Pythonic_Code.py
+++ Lec1-1_Pythonic_Code.py
@@ -5,30 +5,25 @@ result = ""
 
 for s in alphabet:
     result += s
-
 print(result)
 
 #pythonic code
 
 alphabet = ["a","b","c","d","e"]
 result = "".join(alphabet)
-
 print(result)
 
 #split string
 
 items = 'zero one two three'.split()
-
 print(items)
 
 example = 'python,c++,java'
-
 print(example.split(","))
 
 #split string and unpacking
 
 example = 'python, c++, java'
-
 a,b,c = example.split(",")
 print(a,b,c)
 
@@ -38,7 +33,6 @@ result = []
 
 for i in range(10):
     result.append(i)
-
 print(result)
 
 #list Complrehension
@@ -54,7 +48,6 @@ print(result)
 word_1 = "Hello"
 word_2 = "World"
 result = [i+j for i in word_1 for j in word_2]
-
 print(result)
 
 #Nested For loop + if
@@ -67,7 +60,6 @@ print(result)
 
 result = [i+j for i in case_1 for j in case_2 if not(i==j)]
 result.sort()
-
 print(result)
 
 #split + list Comprehension
@@ -79,3 +71,72 @@ stuff = [[w.upper(),w.lower(),len(w)] for w in words]
 
 for i in stuff:
     print(i)
+
+#for loop + zip
+
+alist = ['a1','a2','a3']
+blist = ['b1','b2','b3']
+
+for a,b in zip(alist,blist):
+    print(a,b)
+
+#list comprehension + zip
+
+a,b,c = zip((1,2,3),(10,20,30),(100,200,300))
+print(a,b,c)
+print([sum(x) for x in zip((1,2,3),(10,20,30),(100,200,300))])
+
+#enumerate + zip
+
+alist = ['a1','a2','a3']
+blist = ['b1','b2','b3']
+
+for i,(a,b) in enumerate(zip(alist,blist)):
+    print(i,a,b)
+
+#lambda function
+
+def f(x,y):
+    return x+y
+print (f(1,4))
+
+f = lambda x,y:x+y
+print(f(1,4))
+
+f = lambda x:x/2
+print(f(3))
+
+f = lambda x:x**2
+print(f(3))
+print((lambda x:x+1)(5))
+
+#map with lambda
+
+ex = [1,2,3,4,5]
+print(list(map(lambda x: x+x, ex)))
+print((map(lambda x: x+x, ex)))
+
+f = lambda x: x ** 2
+print(map(f, ex))
+for i in map(f, ex):
+    print(i)
+
+result = map(f, ex)
+print(result)
+print(next(result))
+
+import sys
+sys.getsizeof(ex)
+sys.getsizeof((map(lambda x: x+x, ex)))
+sys.getsizeof(list(map(lambda x: x+x, ex)))
+
+#reduce with lambda
+
+from functools import reduce
+print(reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+
+def factorial(n):
+    return reduce(
+            lambda x, y: x*y, range(1, n+1))
+
+print(factorial(5))
